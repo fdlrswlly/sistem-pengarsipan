e1983d0de8ee348068bb5dfa647c2c0d7039121a
fdlrswlly <fadhly.aulia123@gmail.com>
2021-10-28 10:39:34 +0700

    update baru

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -10,7 +10,6 @@
                 <h3>Dashboard</h3>
             </div>
             <div class="clearfix"></div>
-            <div class="row">
                 <div class="col-md-12">
                     <div class="">
                         <div class="x_content">
